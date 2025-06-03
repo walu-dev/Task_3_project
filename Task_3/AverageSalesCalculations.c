763b0b0c0e9a29ea191b8d779090bc8ce40ecb5d
walu-dev <jonathanenoch11@gmail.com>
2025-06-03 20:38:23 +0300

    Calculate average sales

--- Task_3/AverageSalesCalculations.c
+++ Task_3/AverageSalesCalculations.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "AverageSalesCalculations.h"
 
+
+void CalculateAverageSales(){
+    int total_sales, days;
+
+    //prompt user to enter total_sales
+    printf("Enter total_sales: \n");
+    scanf("%d", &total_sales);
+
+    //prompt user to enter number of days
+    printf("Enter days: ");
+    scanf("%d", &days);
+
+    double average_sales = (double)(total_sales /days);
+    printf("The Average sales is: %.2lf\n", average_sales);
+
+}
