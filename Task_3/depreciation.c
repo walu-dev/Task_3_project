f647491424eadffd01290693e8f32225791c5651
walu-dev <jonathanenoch11@gmail.com>
2025-06-03 21:49:56 +0300

    Calculate depreciation

--- Task_3/depreciation.c
+++ Task_3/depreciation.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include "depreciation.h"
+
+void CalculateDepreciation(){
+    int usefu_life;
+    double residual_value, asset_cost;
+
+    //prompt user to enter asset cost
+    printf("Enter asset_cost: ");
+    scanf("%lf", &asset_cost);
+
+    //prompt user to enter residual_value
+    printf("Enter residual_value: ");
+    scanf("%lf", &residual_value);
+
+    //prompt user to enter usefu_life
+    printf("Enter usefu_life: ");
+    scanf("%d", &usefu_life);
+
+    double depreciation = (asset_cost - residual_value)/ (double)usefu_life;
+    printf("The depreciation is: %.2lf\n", depreciation);
+
+}
 
--- Task_3/main.c
+++ Task_3/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "AverageSalesCalculations.h"
+#include "CalculateCommission.h"
+#include "depreciation.h"
 
 int main()
 {
     //CalculateAverageSales();
-    CalculatedCommission();
+    //CalculatedCommission();
+    CalculateDepreciation();
     return 0;
 }
