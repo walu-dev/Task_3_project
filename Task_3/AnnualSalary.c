3a657410572fdce2ff8fb571c8420786e43d6156
walu-dev <jonathanenoch11@gmail.com>
2025-06-04 11:48:19 +0300

    Calculate annual salary

--- Task_3/AnnualSalary.c
+++ Task_3/AnnualSalary.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include "AnnualSalary.h"
 
+void CalculateAnnualSalary(){
+    double hourly_wage;
+    int hours_per_week, weeks_per_year, annual_salary;
+
+    //prompt user to enter hourly wage
+    printf("Enter the hourly Wage: \n");
+    scanf("%lf", &hourly_wage);
+
+    //prompt user to enter number of weeks
+    printf("Enter the number of weeks per year: \n");
+    scanf("%d", &weeks_per_year);
+
+    //prompt user to enter number of hours
+    printf("Enter the working hours per week: \n");
+    scanf("%d", &hours_per_week);
+
+    annual_salary = (int)hourly_wage * hours_per_week * weeks_per_year;
+    printf("The annual salary is: %d", annual_salary);
+}
